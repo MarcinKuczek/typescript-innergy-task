2edf1723ad1c0e1c39b483768ad467b45d6fe75e
Marcin Kuczek (MTV) <MTV@kmd.dk>
2023-11-09 18:42:30 +0100

    Description to Prices model

--- src/models/PricesByYearsMap.ts
+++ src/models/PricesByYearsMap.ts
@@ -1,5 +1,8 @@
 import { ServiceYear } from ".."
 
+// This is a simple class that simulates Prices stored in a DB
+// In a real life this should be changed to split Services and their prices each year
+// since we don't have real backend here we can simplify that part
 export class PricesByYearsMap
     {
         constructor(
