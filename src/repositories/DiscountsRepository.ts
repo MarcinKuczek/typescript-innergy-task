4c626a5d3e39f1991fcdbfc33baa1024b3b388ee
Marcin Kuczek (MTV) <MTV@kmd.dk>
2023-11-09 18:43:54 +0100

    Repositories that simulates our DB store with data

---
+++ src/repositories/DiscountsRepository.ts
@@ -0,0 +1,23 @@
+import { Discount } from "../models/Discount";
+
+// Simulates discounts saved in the database
+// This is designed to make rules maintanable in the future 
+export const DiscountsRepository: Discount[] =
+        [
+            // rule: wedding session costs regularly $600, but in a package with photography during the wedding or with a video recording it costs $300,
+            new Discount(2020, 300, [ "WeddingSession", "Photography"], [ "WeddingSession" ] ),
+            new Discount(2021, 300, ["WeddingSession", "Photography"], ["WeddingSession" ] ),
+            new Discount(2022, 300,["WeddingSession","Photography"] , ["WeddingSession" ]),
+
+            new Discount(2020, 300, [ "WeddingSession", "VideoRecording"], [ "WeddingSession" ] ),
+            new Discount(2021, 300, ["WeddingSession", "VideoRecording"], ["WeddingSession" ] ),
+            new Discount(2022, 300,["WeddingSession","VideoRecording"] , ["WeddingSession" ]),
+
+            // rule: wedding session is free if the client chooses Photography during the wedding in 2022,
+            new Discount(2022, 0, ["WeddingSession", "Photography"], ["WeddingSession"]),
+
+            // rule: package of photography + video costs less: $2200 in 2020, $2300 in 2021 and $2500 in 2022,
+            new Discount(2020, 2200, ["Photography", "VideoRecording"],["Photography", "VideoRecording" ]),
+            new Discount(2021, 2300, ["Photography", "VideoRecording"],["Photography", "VideoRecording" ]),
+            new Discount(2022, 2500, ["Photography", "VideoRecording"],["Photography", "VideoRecording" ]),
+        ];
---
+++ src/repositories/PricesRepository.ts
@@ -0,0 +1,8 @@
+import { PricesByYearsMap } from "../models/PricesByYearsMap";
+
+export const PricesRepository: PricesByYearsMap[] =
+        [
+            new PricesByYearsMap(2020, 1700, 1700, 600, 300, 400),
+            new PricesByYearsMap(2021, 1800, 1800, 600, 300, 400),
+            new PricesByYearsMap(2022, 1900, 1900, 600, 300, 400)
+        ];
